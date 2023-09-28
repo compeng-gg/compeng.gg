8748bfc66d5d3ad9b654053a545a8da8d2b2a90f
Jonathan Eyolfson <jon@eyolfson.com>
2023-09-28 09:37:44 -0400

    Added robots.txt.

--- compeng_gg/urls.py
+++ compeng_gg/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="homepage.html"), name="homepage"),
+    path("", TemplateView.as_view(template_name="homepage.html"),
+         name="homepage"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain")),
     path("admin/", admin.site.urls),
 ]
