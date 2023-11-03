846ee0ce979880db384fe18c7896561befd02f39
Jonathan Eyolfson <jon@eyolfson.com>
2023-11-03 07:32:05 -0400

    Change name in admin display.

--- queue_webhook/apps.py
+++ queue_webhook/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class QueueWebhookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'queue_webhook'
+    verbose_name = 'Queue (Webhook)'
