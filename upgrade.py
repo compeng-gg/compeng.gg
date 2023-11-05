6cf2e1416de7a5ab39b32a1201377ebeb45ae2e2
Jonathan Eyolfson <jon@eyolfson.com>
2023-11-05 18:19:53 -0500

    Added upgrade requirements to upgrade script.

--- upgrade.py
+++ upgrade.py
@@ -28,11 +28,31 @@ def update_iosevka(base_dir, fonts_dir):
     subprocess.run(['mv', iosevka_dir / 'woff2' / 'iosevka-italic.woff2', fonts_dir], check=True, cwd=base_dir)
     subprocess.run(['rm', '-rf', iosevka_dir], check=True, cwd=base_dir)
 
+def update_requirements(base_dir):
+    subprocess.run(['rm', '-rf', 'venv'], check=True, cwd=base_dir)
+    subprocess.run(['python3', '-m', 'venv', 'venv'], check=True, cwd=base_dir)
+    venv_dir = base_dir / 'venv'
+    venv_env = os.environ.copy()
+    venv_env['VIRTUAL_ENV'] = venv_dir
+    venv_env['PATH'] = f"{venv_dir / 'bin'}:{venv_env['PATH']}"
+    subprocess.run(['pip', 'install', '-U', 'pip'],
+                   check=True, cwd=base_dir, env=venv_env)
+    subprocess.run(['pip', 'install', 'django'],
+                   check=True, cwd=base_dir, env=venv_env)
+    subprocess.run(['pip', 'install', 'social-auth-app-django'],
+                   check=True, cwd=base_dir, env=venv_env)
+    p = subprocess.run(['pip', 'freeze'],
+                       capture_output=True, check=True, cwd=base_dir,
+                       env=venv_env, text=True)
+    with open(base_dir / 'requirements.txt', 'w') as f:
+        f.write(p.stdout)
+
 def main():
     base_dir = pathlib.Path(__file__).resolve().parent
     fonts_dir = base_dir / 'compeng_gg' / 'static' / 'fonts'
     os.makedirs(fonts_dir, exist_ok=True)
 
+    update_requirements(base_dir)
     update_inter(base_dir, fonts_dir)
     update_iosevka(base_dir, fonts_dir)
 
