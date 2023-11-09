c383929ec40f6fe491f1bd8447c6458d8c8a2204
Jonathan Eyolfson <jon@eyolfson.com>
2023-11-09 12:20:57 -0500

    Change message.

--- queue_webhook/management/commands/runqueue.py
+++ queue_webhook/management/commands/runqueue.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
 
         host, lock = SERVERS[index]
         with lock:
-            self.stdout.write(f'{task} running on {host}')
+            self.stdout.write(f'{task} sent to {host}')
             cmd = ['ssh', host, '/opt/compeng.gg/venv/bin/python', '/opt/compeng.gg/manage.py', 'runtask', str(task.id)]
             # cmd = ['python', 'manage.py', 'runtask', str(task.id)]
             p = subprocess.run(cmd, cwd=settings.BASE_DIR)
