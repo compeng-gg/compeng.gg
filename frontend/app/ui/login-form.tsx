dcc9f54fc3778f93f67764adac89c71e830607eb
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-04 14:21:32 -0400

    Move from Vite to Next.JS.

--- frontend/app/ui/login-form.tsx
+++ frontend/app/ui/login-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SyntheticEvent, useState } from "react";
 
 // const API_URL = 'https://localhost:8080/api/';
@@ -31,7 +33,7 @@ function LoginForm() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "http://localhost:5731",
+                "Access-Control-Allow-Origin": "http://localhost:3000",
                 "X-CSRFToken": csrftoken,
             },
             credentials: "include",
@@ -42,13 +44,21 @@ function LoginForm() {
     .then((data) => console.log(data));
   }
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <label htmlFor="username">Username:</label>
-      <input id="username" type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
-      <label htmlFor="password">Password:</label>
-      <input id="password" type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
-      <button type="submit">Login</button>
-    </form>
+    <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">Username:</label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password:</label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
