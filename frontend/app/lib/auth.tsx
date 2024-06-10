31f3cd73c23472bafb18483e2acd59ad7723fb33
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-10 12:10:18 -0400

    Added environment variables for Codespaces.

--- frontend/app/lib/auth.tsx
+++ frontend/app/lib/auth.tsx
@@ -33,10 +33,10 @@ export default function withAuth(Component: any) {
     }
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <div>
+          <div>
             <h1 className="mb-4 font-black text-5xl">CompEng.gg</h1>
             <LoginForm />
-        </div>
+          </div>
         </main>
     );
   }
