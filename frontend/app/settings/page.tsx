78b97078e3bd6aec7a2b23c9b6730034b04ba3a5
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-13 15:57:03 -0400

    Remove bad auth from settings.

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,14 +1,7 @@
-import { cookies } from "next/headers";
-import Image from "next/image";
-import LoginForm from '@/app/ui/login-form';
-import withAuth from "@/app/lib/auth";
-
-function Page({username}: {username: string}) {
+export default function Page({username}: {username: string}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="mb-4 font-black text-5xl">@{username} Settings</h1>
     </main>
   );
 }
-
-export default withAuth(Page);
