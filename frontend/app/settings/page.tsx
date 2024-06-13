2ce6b856580eb02943d03e625791217571b9f373
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-13 15:57:54 -0400

    Remove username from settings.

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,7 +1,7 @@
-export default function Page({username}: {username: string}) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="mb-4 font-black text-5xl">@{username} Settings</h1>
+      <h1 className="mb-4 font-black text-5xl">Settings</h1>
     </main>
   );
 }
