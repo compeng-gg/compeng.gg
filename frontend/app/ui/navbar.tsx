e1f417f2e35c48242f7b34f2c5290b7fa4a39d33
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-17 12:29:28 -0400

    Updated Dockerfiles.

--- frontend/app/ui/navbar.tsx
+++ frontend/app/ui/navbar.tsx
@@ -5,7 +5,7 @@ import LogoutButton from '@/app/ui/logout-button';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-4">
+    <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-2">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link href="/" className="font-black text-xl tracking-tight">CompEng.gg</Link>
       </div>
