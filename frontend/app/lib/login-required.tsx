945d4615f87bbf7ca0e775f61d64f275cd5a4531
Jonathan Eyolfson <jon@eyolfson.com>
2024-06-18 09:57:31 -0400

    Reduce padding on login required.

--- frontend/app/lib/login-required.tsx
+++ frontend/app/lib/login-required.tsx
@@ -16,7 +16,7 @@ export default function LoginRequired({
 
   if (jwt === undefined) {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <main className="flex min-h-screen flex-col items-center justify-center p-8">
         <div>
           <h1 className="mb-4 font-black text-5xl">CompEng.gg</h1>
           <p className="mb-2">Already have an account?</p>
