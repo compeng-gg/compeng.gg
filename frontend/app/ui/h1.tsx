3ab2c0b22ea5894ffdb53a46614d30be09a3e78d
Jonathan Eyolfson <jon@eyolfson.com>
2024-07-28 10:40:05 -0400

    Change headings to use bold text instead.

--- frontend/app/ui/h1.tsx
+++ frontend/app/ui/h1.tsx
@@ -1,6 +1,6 @@
 function H1({children}: {children: React.ReactNode}) {
     return (
-        <h1 className="font-black text-4xl">{children}</h1>
+        <h1 className="font-bold text-4xl">{children}</h1>
     );
 }
 
--- frontend/app/ui/h2.tsx
+++ frontend/app/ui/h2.tsx
@@ -1,6 +1,6 @@
 function H2({children}: {children: React.ReactNode}) {
     return (
-        <h1 className="font-black text-2xl">{children}</h1>
+        <h1 className="font-bold text-2xl">{children}</h1>
     );
 }
 
