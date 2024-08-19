c36451168b6821115fafcc99e8a7948b4c750477
whykalashwhy <kalashbhaiya@gmail.com>
2024-08-19 15:23:44 +0000

    made the navbar translucent, changed page background

--- frontend/app/ui/navbar.tsx
+++ frontend/app/ui/navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
     closeDropdown();
   };
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-zinc-900 via-zinc-800 to-zinc-900 p-2 relative">
+    <nav className="flex items-center justify-between flex-wrap bg-opacity-50 backdrop-blur-lg shadow-md p-2 relative">
       <div className="hidden lg:flex items-center flex-shrink-0 text-white mr-6 transition transform active:scale-95">
         <Link href="/" className="font-black text-xl tracking-tight">CompEng.gg</Link>
       </div>
