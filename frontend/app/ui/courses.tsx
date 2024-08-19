4cfe813b3146e9a569b148ff28c361f99d193245
whykalashwhy <kalashbhaiya@gmail.com>
2024-08-19 15:08:50 +0000

    minor courses changes

--- frontend/app/ui/courses.tsx
+++ frontend/app/ui/courses.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import { useState } from 'react';
 
 interface Offering {
   slug: string;
@@ -12,25 +13,32 @@ const offerings: Offering[] = [
 ];
 
 function Courses() {
+  const [clickedIndex, setClickedIndex] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    setClickedIndex(index);
+  };
+
   return (
     <>
       <h2 className="text-3xl font-bold mb-4">Courses</h2>
      
-        {offerings.length > 0 ? (
-          offerings.map((offering, i) => (
-            <div key={i} className="p-4 mb-4 bg-zinc-900 border border-gray-600 rounded-xl">
-              <Link href={`/${offering.slug}/`} className="text-blue-500 hover:underline">
+      {offerings.length > 0 ? (
+        offerings.map((offering, i) => (
+          <div key={i} className={`p-4 mb-4 bg-zinc-900 border border-gray-600 rounded-xl transition-transform duration-300 ${clickedIndex === i ? 'scale-95' : 'scale-100'}`}
+            onClick={() => handleClick(i)}>
+            <Link href={`/${offering.slug}/`} className="text-blue-500 hover:underline">
               <p className="text-2xl mb-1 text-blue-500 font-bold">{offering.name}</p>
-              </Link>
-              some info here
-            </div>
-          ))
-        ) : (
-          <p className="text-gray-500">No current courses</p>
-        )}
-  
+            </Link>
+            some info here
+          </div>
+        ))
+      ) : (
+        <p className="text-gray-500">No current courses</p>
+      )}
     </>
   );
 }
+
 
 export default Courses;
