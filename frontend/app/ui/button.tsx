d1c724090768a91bf2b063d4e3c3f59ad6c856e2
whykalashwhy <kalashbhaiya@gmail.com>
2024-08-21 18:06:58 +0000

    button, courses, made more responsive

--- frontend/app/ui/button.tsx
+++ frontend/app/ui/button.tsx
@@ -6,7 +6,8 @@ interface ButtonProps {
 }
 
 function Button({ kind, onClick, type, children }: ButtonProps) {
-  const baseClasses = 'px-4 py-2 rounded text-sm text-white cursor-pointer transition transform active:scale-95';
+  const baseClasses = 'px-4 py-2 rounded text-sm text-white cursor-pointer transition-transform transform active:scale-95 bg-blue-500 hover:bg-blue-700';
+
   const kindClasses =  kind === 'primary'
     ? 'bg-blue-500'
     : 'bg-gray-700';
