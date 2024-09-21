5538dcefb0f2b8b2dd86c9bb5f546fbbf638719b
Jonathan Eyolfson <jon@eyolfson.com>
2024-09-21 17:51:14 -0400

    Added ece454 check.

--- backend/courses/utils.py
+++ backend/courses/utils.py
@@ -7,36 +7,40 @@ def get_data_for_push(push):
 
     name = push.payload['repository']['name']
     ece344_prefix = '2024-fall-ece344-'
+    ece454_prefix = '2024-fall-ece454-'
 
-    if name.startswith(ece344_prefix):
-        username = name[len(ece344_prefix):]
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            return data
-        data['user'] = user
-        role = Role.objects.get(
-            kind=Role.Kind.STUDENT, offering__course__slug='ece344'
-        )
-        try:
-            enrollment = Enrollment.objects.get(user=user, role=role)
-        except Enrollment.DoesNotExist:
-            return data
-        data['enrollment'] = enrollment
+    # TODO, this should be a lot better
+    if not (name.startswith(ece344_prefix) or name.startswith(ece454_prefix)):
+        return data
 
-        offering = role.offering
-        assignments = []
-        for assignment in offering.assignment_set.all():
-            files = assignment.files
-            found = False
-            for commit in push.payload['commits']:
-                for modified_file in commit['modified']:
-                    if modified_file in files:
-                        found = True
-                        assignments.append(assignment)
-                        break
-                if found:
+    username = name[len(ece344_prefix):]
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return data
+    data['user'] = user
+    role = Role.objects.get(
+        kind=Role.Kind.STUDENT, offering__course__slug='ece344'
+    )
+    try:
+        enrollment = Enrollment.objects.get(user=user, role=role)
+    except Enrollment.DoesNotExist:
+        return data
+    data['enrollment'] = enrollment
+
+    offering = role.offering
+    assignments = []
+    for assignment in offering.assignment_set.all():
+        files = assignment.files
+        found = False
+        for commit in push.payload['commits']:
+            for modified_file in commit['modified']:
+                if modified_file in files:
+                    found = True
+                    assignments.append(assignment)
                     break
-        data['assignments'] = assignments
+            if found:
+                break
+    data['assignments'] = assignments
 
     return data
