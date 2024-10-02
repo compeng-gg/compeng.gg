f88642298dbbef1ccdfd4c1a401359b2b3733a1f
Abdullah Qureshi <abd.qureshi@mail.utoronto.ca>
2024-10-02 16:18:28 -0400

    added slug for get teams

--- backend/api/v0/urls.py
+++ backend/api/v0/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('teams/join/manage/', teams_api.manage_join_team_request),
     path('teams/delete/', teams_api.delete_team),
     path('teams/leave/', teams_api.leave_team),
-    path('teams/get/<slug:slug>', teams_api.teams),
+    path('teams/<slug:slug>/', teams_api.teams),
     path('teams/create/', teams_api.create_team),
 
     path('github/webhook/', github_webhook),
