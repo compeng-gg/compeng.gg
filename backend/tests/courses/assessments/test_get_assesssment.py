891d3f9da43563cb22d54e1e06de4d7e3df0fc53
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-11-22 02:24:44 -0500

    Unit tests for assessment retrieval / submission

--- backend/tests/courses/assessments/test_get_assesssment.py
+++ backend/tests/courses/assessments/test_get_assesssment.py
@@ -1,83 +1,327 @@
-from tests.utils import create_offering, TestCasesWithUserAuth
-from django.contrib.auth.models import User
+from tests.utils import TestCasesWithUserAuth
 import courses.models as db
 from datetime import (
     datetime,
-    timedelta,
+    timezone,
 )
+from tests.utils import (
+    create_assessment
+)
+from rest_framework import status
 
 
 class GetAssessmentTests(TestCasesWithUserAuth):
     def test_creates_new_submission_on_first_request_happy_path(self):
-
-
-        offering = create_offering()
+        requesting_user_id = self.user.id
         
-        student_role = db.Role.objects.create(kind=db.Role.Kind.STUDENT, offering=offering)
-        
-        requesting_user = self.user
-
-        enrollment = db.Enrollment.objects.create(
-            user=requesting_user,
-            role=student_role,
-        )
-
-        print(f"Create enrol for {requesting_user.id}")
-
-        start_datetime = datetime.now()
-        end_datetime = start_datetime + timedelta(hours=1)
-
-        assessment = db.Assessment.objects.create(
-            start_datetime=start_datetime,
-            end_datetime=end_datetime,
-            offering=offering,
-        )
-
-        db.CodingQuestion.objects.create(
-            assessment=assessment,
-            prompt="Explain this",
-            order=1,
-        )
-
-        db.MultipleChoiceQuestion.objects.create(
-            assessment=assessment,
-            prompt="Explain this",
-            order=1,
-            options=[
-                "test"
-            ],
-            correct_option_index=0
-        )
-
-        db.WrittenResponseQuestion.objects.create(
-            assessment=assessment,
-            prompt="Explain this",
-            order=1,
-        )
-
-        self.client.force_login(requesting_user)
-        print(f"Test client logged in user: {self.client.session.get('_auth_user_id')}")
+        assessment = create_assessment(user_id=requesting_user_id)
 
         # Initially no submission should exist
         self.assertFalse(db.AssessmentSubmission.objects.filter(
-            enrollment=enrollment,
+            user_id=requesting_user_id,
             assessment=assessment
         ).exists())
 
         response = self.client.get(
             f"/api/v0/assessments/{assessment.id}/",
         )
-        print(response.json())
+
         self.assertEqual(
-            response.status_code, 200
+            response.status_code, status.HTTP_200_OK
         )
 
+        # After retrieving assessment for the first time, AssessmentSubmission object should be created
         self.assertTrue(db.AssessmentSubmission.objects.filter(
-            enrollment=enrollment,
+            user_id=requesting_user_id,
             assessment=assessment
         ).exists())
+        
+    def test_returns_saved_coding_solution(self):
+        requesting_user_id = self.user.id
+        
+        assessment = create_assessment(user_id=requesting_user_id)
+        
+        assessment_submission = db.AssessmentSubmission.objects.create(
+            user_id=requesting_user_id,
+            assessment=assessment,
+            start_datetime=datetime.now(timezone.utc)
+        )
 
-        print('dd')
-        print(response.json())
+        coding_question = db.CodingQuestion.objects.create(
+            assessment=assessment,
+            prompt='Write a function that prints "Hello World!" in Python',
+            order=1,
+            points=20,
+            programming_language=db.CodingQuestion.ProgrammingLanguage.PYTHON,
+        )
+        
+        coding_answer = db.CodingAnswer.objects.create(
+            assessment_submission=assessment_submission,
+            question=coding_question,
+            solution='print("Hello World!")'
+        )
+        
+        response = self.client.get(
+            f"/api/v0/assessments/{assessment.id}/",
+        )
 
-        exit()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        expected_body = {
+            'title': assessment.title,
+            'end_unix_timestamp': int(assessment.end_datetime.timestamp()),
+            'questions': [
+                {
+                    'prompt': coding_question.prompt,
+                    'question_type': 'CODING',
+                    'points': coding_question.points,
+                    'programming_language': str(coding_question.programming_language),
+                    'solution': coding_answer.solution
+                }
+            ]
+        }
+        
+        self.assertDictEqual(
+            response.json(), expected_body
+        )
+        
+    def test_returns_saved_multiple_choice_solution(self):
+        requesting_user_id = self.user.id
+        
+        assessment = create_assessment(user_id=requesting_user_id)
+        
+        assessment_submission = db.AssessmentSubmission.objects.create(
+            user_id=requesting_user_id,
+            assessment=assessment,
+            start_datetime=datetime.now(timezone.utc)
+        )
+
+        multiple_choice_question = db.MultipleChoiceQuestion.objects.create(
+            assessment=assessment,
+            prompt='Choose the animal that flies',
+            order=1,
+            points=3,
+            options=[
+                'Deer',
+                'Bird',
+                'Shark'
+            ],
+            correct_option_index=1
+        )
+        
+        multiple_choice_answer = db.MultipleChoiceAnswer.objects.create(
+            assessment_submission=assessment_submission,
+            question=multiple_choice_question,
+            selected_answer_index=0
+        )
+        
+        response = self.client.get(
+            f"/api/v0/assessments/{assessment.id}/",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        expected_body = {
+            'title': assessment.title,
+            'end_unix_timestamp': int(assessment.end_datetime.timestamp()),
+            'questions': [
+                {
+                    'prompt': multiple_choice_question.prompt,
+                    'question_type': 'MULTIPLE_CHOICE',
+                    'points': multiple_choice_question.points,
+                    'options': multiple_choice_question.options,
+                    'selected_answer_index': multiple_choice_answer.selected_answer_index
+                },
+            ]
+        }
+        
+        self.assertDictEqual(
+            response.json(), expected_body
+        )
+        
+    def test_returns_saved_checkbox_solution(self):
+        requesting_user_id = self.user.id
+        
+        assessment = create_assessment(user_id=requesting_user_id)
+        
+        assessment_submission = db.AssessmentSubmission.objects.create(
+            user_id=requesting_user_id,
+            assessment=assessment,
+            start_datetime=datetime.now(timezone.utc)
+        )
+
+        checkbox_question = db.CheckboxQuestion.objects.create(
+            assessment=assessment,
+            prompt='Choose all positive numbers',
+            order=2,
+            points=4,
+            options=['-1', '-2', '3', '4'],
+            correct_option_indices=[2, 3]
+        )
+        
+        checkbox_answer = db.CheckboxAnswer.objects.create(
+            assessment_submission=assessment_submission,
+            question=checkbox_question,
+            selected_answer_indices=[0, 1, 2]
+        )
+        
+        response = self.client.get(
+            f"/api/v0/assessments/{assessment.id}/",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        expected_body = {
+            'title': assessment.title,
+            'end_unix_timestamp': int(assessment.end_datetime.timestamp()),
+            'questions': [
+                {
+                    'prompt': checkbox_question.prompt,
+                    'question_type': 'CHECKBOX',
+                    'points': checkbox_question.points,
+                    'options': checkbox_question.options,
+                    'selected_answer_indices': checkbox_answer.selected_answer_indices
+                },
+            ]
+        }
+        
+        self.assertDictEqual(
+            response.json(), expected_body
+        )
+        
+    def test_returns_saved_written_response_solution(self):
+        requesting_user_id = self.user.id
+        
+        assessment = create_assessment(user_id=requesting_user_id)
+        
+        assessment_submission = db.AssessmentSubmission.objects.create(
+            user_id=requesting_user_id,
+            assessment=assessment,
+            start_datetime=datetime.now(timezone.utc)
+        )
+
+        written_response_question = db.WrittenResponseQuestion.objects.create(
+            assessment=assessment,
+            prompt="Write a poem",
+            order=1,
+            points=5,
+            max_length=450
+        )
+        
+        written_response_answer = db.WrittenResponseAnswer.objects.create(
+            assessment_submission=assessment_submission,
+            question=written_response_question,
+            response="Noooooo"
+        )
+        
+        response = self.client.get(
+            f"/api/v0/assessments/{assessment.id}/",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        expected_body = {
+            'title': assessment.title,
+            'end_unix_timestamp': int(assessment.end_datetime.timestamp()),
+            'questions': [
+                {
+                    'prompt': written_response_question.prompt,
+                    'question_type': 'WRITTEN_RESPONSE',
+                    'points': written_response_question.points,
+                    'max_length': written_response_question.max_length,
+                    'response': written_response_answer.response
+                },
+            ]
+        }
+        
+        self.assertDictEqual(
+            response.json(), expected_body
+        )
+        
+    def test_returns_question_types_in_order_happy_path(self):
+        requesting_user_id = self.user.id
+        
+        assessment = create_assessment(user_id=requesting_user_id)
+
+        coding_question = db.CodingQuestion.objects.create(
+            assessment=assessment,
+            prompt='Write a function that prints "Hello World!" in Python',
+            order=4,
+            points=20,
+            programming_language=db.CodingQuestion.ProgrammingLanguage.PYTHON,
+        )
+
+        multiple_choice_question = db.MultipleChoiceQuestion.objects.create(
+            assessment=assessment,
+            prompt='Choose the animal that flies',
+            order=1,
+            points=3,
+            options=[
+                'Deer',
+                'Bird',
+                'Shark'
+            ],
+            correct_option_index=1
+        )
+
+        written_response_question = db.WrittenResponseQuestion.objects.create(
+            assessment=assessment,
+            prompt="Write a poem",
+            order=3,
+            points=5,
+            max_length=450
+        )
+        
+        checkbox_question = db.CheckboxQuestion.objects.create(
+            assessment=assessment,
+            prompt='Choose all positive numbers',
+            order=2,
+            points=4,
+            options=['-1', '-2', '3', '4'],
+            correct_option_indices=[2, 3]
+        )
+
+        response = self.client.get(
+            f"/api/v0/assessments/{assessment.id}/",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_body = {
+            'title': assessment.title,
+            'end_unix_timestamp': int(assessment.end_datetime.timestamp()),
+            'questions': [
+                {
+                    'prompt': multiple_choice_question.prompt,
+                    'question_type': 'MULTIPLE_CHOICE',
+                    'points': multiple_choice_question.points,
+                    'options': multiple_choice_question.options,
+                    'selected_answer_index': None
+                },
+                {
+                    'prompt': checkbox_question.prompt,
+                    'question_type': 'CHECKBOX',
+                    'points': checkbox_question.points,
+                    'options': checkbox_question.options,
+                    'selected_answer_indices': None
+                },
+                {
+                    'prompt': written_response_question.prompt,
+                    'question_type': 'WRITTEN_RESPONSE',
+                    'points': written_response_question.points,
+                    'max_length': written_response_question.max_length,
+                    'response': None
+                },
+                {
+                    'prompt': coding_question.prompt,
+                    'question_type': 'CODING',
+                    'points': coding_question.points,
+                    'programming_language': str(coding_question.programming_language),
+                    'solution': None
+                }
+            ]
+        }
+        
+        self.assertDictEqual(
+            response.json(), expected_body
+        )
