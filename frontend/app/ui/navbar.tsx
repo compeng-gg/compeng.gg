9a9bd6a5487a6c9b3f951b47d454ac3946111f66
whykalashwhy <kalashbhaiya@gmail.com>
2024-11-30 00:26:36 -0500

    figured out dropdowns (semi error less)

--- frontend/app/ui/navbar.tsx
+++ frontend/app/ui/navbar.tsx
@@ -5,53 +5,53 @@ import Link from 'next/link';
 import LogoutButton from '@/app/ui/logout-button';
 
 export default function Navbar() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const dropdownRef = useRef<HTMLDivElement>(null);  // Ref for the dropdown
+  // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // const dropdownTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  // const dropdownRef = useRef<HTMLDivElement>(null);  
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prevState) => !prevState);
-  };
+  // const toggleDropdown = () => {
+  //   setIsDropdownOpen((prevState) => !prevState); //chnage state, toggle
+  // };
 
-  const openDropdown = () => {
-    if (dropdownTimeoutRef.current) {
-      clearTimeout(dropdownTimeoutRef.current);
-      dropdownTimeoutRef.current = null;
-    }
-    setIsDropdownOpen(true);
-  };
+  // const openDropdown = () => {
+  //   if (dropdownTimeoutRef.current) {
+  //     clearTimeout(dropdownTimeoutRef.current); //c
+  //     dropdownTimeoutRef.current = null;
+  //   }
+  //   setIsDropdownOpen(true);
+  // };
 
-  const closeDropdown = () => {
-    dropdownTimeoutRef.current = setTimeout(() => {
-      setIsDropdownOpen(false);
-    }, 300);
-  };
+  // const closeDropdown = () => {
+  //   dropdownTimeoutRef.current = setTimeout(() => {
+  //     setIsDropdownOpen(false);
+  //   }, 300);
+  // };
 
-  const handleMouseEnter = () => {
-    if (dropdownTimeoutRef.current) {
-      clearTimeout(dropdownTimeoutRef.current);
-      dropdownTimeoutRef.current = null;
-    }
-    openDropdown();
-  };
+  // const handleMouseEnter = () => {
+  //   if (dropdownTimeoutRef.current) {
+  //     clearTimeout(dropdownTimeoutRef.current);
+  //     dropdownTimeoutRef.current = null;
+  //   }
+  //   openDropdown();
+  // };
 
-  const handleMouseLeave = () => {
-    closeDropdown();
-  };
+  // const handleMouseLeave = () => {
+  //   closeDropdown();
+  // };
 
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsDropdownOpen(false);
-      }
-    }
+  // useEffect(() => {
+  //   function handleClickOutside(event: MouseEvent) {
+  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+  //       setIsDropdownOpen(false);
+  //     }
+  //   }
 
-    document.addEventListener('mousedown', handleClickOutside);
+  //   document.addEventListener('mousedown', handleClickOutside);
 
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClickOutside);
+  //   };
+  // }, []);
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-zinc-900 transition-colors duration-200 ease-in-out p-2 relative hover:bg-zinc-800">
@@ -59,51 +59,22 @@ export default function Navbar() {
         <Link href="/" className="font-black text-xl tracking-tight">CompEng.gg</Link>
       </div>
 
-      {/* Mobile Profile Button */}
-      <div className="block lg:hidden ml-auto relative">
+      <div className="relative ml-auto">
         <button 
           className="flex items-center justify-center w-8 h-8 rounded-full border border-zinc-800 text-zinc-200 hover:text-white hover:border-white transition transform active:scale-95"
-          onClick={toggleDropdown}
+          // onClick={toggleDropdown}
+          // onMouseEnter={handleMouseEnter}
+          // onMouseLeave={handleMouseLeave}
         >
           <svg className="fill-current h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <title>Profile</title>
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.31 0-6 2.69-6 6v1h12v-1c0-3.31-2.69-6-6-6z"/>
           </svg>
         </button>
-        {isDropdownOpen && (
+        {/* {isDropdownOpen &&  */
+        (
           <div 
-            ref={dropdownRef}  
-            className="absolute right-2 mt-2 w-48 bg-zinc-800 rounded-lg shadow-lg flex flex-col items-center"
-          >
-            <Link href="/" className="w-full px-4 py-2 text-zinc-100 text-center hover:bg-zinc-700 transition transform active:scale-95">
-              CompEng.gg
-            </Link>
-            <Link href="/settings/" className="w-full px-4 py-2 text-zinc-100 text-center hover:bg-zinc-700 transition transform active:scale-95">
-              Settings
-            </Link>
-            <LogoutButton />
-          </div>
-        )}
-      </div>
-
-      {/* Desktop Profile Button */}
-      <div 
-        className="hidden lg:block relative ml-auto" 
-        onMouseEnter={handleMouseEnter} 
-        onMouseLeave={handleMouseLeave}
-        onClick={toggleDropdown}
-      >
-        <button 
-          className="flex items-center justify-center w-8 h-8 rounded-full border border-zinc-800 text-zinc-200 hover:text-white hover:border-white transition transform active:scale-95"
-        >
-          <svg className="fill-current h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <title>Profile</title>
-            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.31 0-6 2.69-6 6v1h12v-1c0-3.31-2.69-6-6-6z"/>
-          </svg>
-        </button>
-        {isDropdownOpen && (
-          <div 
-            ref={dropdownRef} 
+            //ref={dropdownRef} 
             className="absolute right-0 mt-2 w-48 bg-zinc-800 rounded-lg shadow-lg flex flex-col items-center transition transform active:scale-95">
             <Link href="/settings/" className="w-full px-4 py-2 text-zinc-100 text-center hover:bg-zinc-700 transition transform active:scale-95">
               Settings
