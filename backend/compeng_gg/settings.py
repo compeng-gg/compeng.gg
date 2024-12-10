4bdb2d982266a3af5b19e6bd13aee4f3c9d34377
Jonathan Eyolfson <jon@eyolfson.com>
2024-12-10 20:33:25 +0000

    Added CSRF.

--- backend/compeng_gg/settings.py
+++ backend/compeng_gg/settings.py
@@ -258,6 +258,7 @@ if BUILTIN_FRONTEND:
     ]
     MIDDLEWARE += [
         'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
     ]
