0bec0aa41ce178dccb06591e4399a2289c7448e5
Jonathan Eyolfson <jon@eyolfson.com>
2024-12-12 14:34:06 -0500

    Disable CSRF.

--- backend/compeng_gg/settings.py
+++ backend/compeng_gg/settings.py
@@ -260,7 +260,7 @@ if BUILTIN_FRONTEND:
     ]
     MIDDLEWARE += [
         'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
+        # 'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
     ]
