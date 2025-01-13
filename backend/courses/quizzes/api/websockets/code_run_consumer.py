cfdd1af077a90113c878bbedb4299777637ba3d2
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-01-12 20:33:58 -0500

    Add auth and invoke runner on coding question websocket

--- backend/courses/quizzes/api/websockets/code_run_consumer.py
+++ backend/courses/quizzes/api/websockets/code_run_consumer.py
@@ -1,81 +1,135 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-import time
-import sys, io
+from urllib.parse import parse_qs
+from typing import Optional
+from rest_framework_simplejwt.tokens import UntypedToken
+from asgiref.sync import sync_to_async
+from runner.utils import create_quiz_build_runner
+import courses.models as db
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import (
+    AnonymousUser,
+    User
+)
+from django.utils import timezone
+from pydantic import BaseModel
 
-def run_python_code_and_get_stdout(code: str):
-    output_buffer = io.StringIO()
 
-    original_stdout = sys.stdout
-    sys.stdout = output_buffer
+class TestCaseExecution(BaseModel):
+    is_public: bool
+    is_passed: bool
+    input: Optional[str]
+    stdout: Optional[str]
+    expected_stdout: Optional[str]
+    stderr: Optional[str]
 
+
+class CodeRunResponse(BaseModel):
+    num_passed: int
+    num_failed: int
+
+    results: list[TestCaseExecution]
+
+
+@database_sync_to_async
+def get_user_from_token(token):
     try:
-        exec(code)
-    finally:
-        # Reset stdout
-        sys.stdout = original_stdout
-
-    # Get the output from the buffer
-    captured_output = output_buffer.getvalue()
-    output_buffer.close()
-
-    # Print the captured output
-    print("Captured Output:")
-    print(captured_output)
-
-    return captured_output
+        valid_data = UntypedToken(token)
+        return User.objects.get(id=valid_data['user_id'])
+    except Exception:
+        return AnonymousUser()
 
 
-def run__add_numbers__function(solution: str, num1: int, num2: int):
-    code = f"""
-{solution}
-value = add_numbers({num1}, {num2})
-print(value)
-"""
-    captured_output = run_python_code_and_get_stdout(code)
-    print(f"Captured output is {captured_output}")
-    return captured_output.strip()
+def do_stuff(quiz_submission, coding_question_id, solution):
+    coding_answer = db.CodingAnswer.objects.get(quiz_submission=quiz_submission)
+        
 
+    if coding_answer is None:
+        coding_answer = db.CodingAnswer.objects.create(
+            quiz_submission=quiz_submission,
+            question_id=coding_question_id,
+            solution=solution,
+            last_updated_at=timezone.now()
+        )
+    else:
+        coding_answer.solution = solution
+        coding_answer.last_updated_at=timezone.now()
+        coding_answer.save()
 
-ADD_NUMBERS__TEST_CASES = [
-    [[1, 2], 3],
-    [[5, 5], 10]
-]
+    db.CodingAnswerExecution.objects.create(
 
-ADD_NUMBERS__QUESTION_ID = "215a939a-1e86-415c-acb2-0bbffcf35e71"
+    )
 
-def run__hello_world__script(solution: str):
-    captured_output = run_python_code_and_get_stdout(solution)
-    print(f"Captured output is {captured_output}")
-    return captured_output.strip()
+@database_sync_to_async
+def create_coding_answer_execution(solution: str, coding_question_id) -> db.CodingAnswerExecution:
+    coding_answer_execution = db.CodingAnswerExecution.objects.create(
+        coding_question_id=coding_question_id,
+        solution=solution
+    )
+    print(f'Create execution {coding_answer_execution.id}')
+    return coding_answer_execution
 
-HELLO_WORLD_TEST_CASES = [
-    [[], "Hello World!"]
-]
+def mark_execution(coding_answer_execution: db.CodingAnswerExecution) -> CodeRunResponse:
+    coding_answer_execution.refresh_from_db()
 
-HELLO_WORLD__QUESTION_ID = "fb597156-6b5c-4721-b635-b1625624e568"
+    results = []
 
-def run_python_test_cases(test_cases, runner_function, solution):
-    num_test_cases_passed = 0
-    num_test_cases_failed = 0
+    executions = coding_answer_execution.test_case_executions.all()
 
-    for test_case, expected_value in test_cases:
-        result = runner_function(solution, *test_case)
+    num_passed = 0
+    num_failed = 0
+    
+    for execution in executions:
+        test_case = execution.test_case
+        expected = test_case.expected_stdout
 
-        print(f"Got {result}, expected {expected_value}")
-        if result == str(expected_value):
-            num_test_cases_passed += 1
+        actual = execution.stdout
+
+        is_passed = expected == actual
+
+        if is_passed:
+            num_passed += 1
         else:
-            num_test_cases_failed += 1
+            num_failed += 1
 
-    return num_test_cases_passed, num_test_cases_failed
+        test_case_execution = TestCaseExecution(
+            is_public=test_case.is_public,
+            is_passed=is_passed,
+            input=test_case.command if test_case.is_public else None,
+            stdout=actual if test_case.is_public else None,
+            expected_stdout=expected if test_case.is_public else None,
+            stderr=execution.stderr if test_case.is_public else None
+        )
+        results.append(test_case_execution)
+
+    return CodeRunResponse(
+        num_passed=num_passed,
+        num_failed=num_failed,
+        results=results
+    )
 
 class CodeRunConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        query_string = self.scope['query_string'].decode('utf-8')
+        query_params = parse_qs(query_string)
+
+        token = query_params.get('token', [None])[0] 
+        
+        print(f"Auth token is {token}")
+
+        user = await get_user_from_token(token)
+
+        self.user = user
+
+        print(f"User is {user}")
+
+        # TODO: reject if bad auth token
+
         kwargs = self.scope['url_route']['kwargs']
         print(f"KWARGS are {kwargs}")
 
         self.quiz_slug = kwargs.get('quiz_slug')
+        self.course_slug = kwargs.get('course_slug')
         self.coding_question_id = str(kwargs.get('coding_question_id'))
         print(f"Coding question id is {self.coding_question_id}")
         
@@ -85,18 +139,14 @@ class CodeRunConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         solution = data['solution']
 
-        if self.coding_question_id == ADD_NUMBERS__QUESTION_ID:
-            num_test_cases_passed, num_test_cases_failed = run_python_test_cases(ADD_NUMBERS__TEST_CASES, run__add_numbers__function, solution)
-        elif self.coding_question_id == HELLO_WORLD__QUESTION_ID:
-            num_test_cases_passed, num_test_cases_failed = run_python_test_cases(HELLO_WORLD_TEST_CASES, run__hello_world__script, solution)
-        else:
-            num_test_cases_passed, num_test_cases_failed = 0, 2
+        print("TEST")
 
-        print(f"Passed {num_test_cases_passed}, failed {num_test_cases_failed}")
+        coding_answer_execution = await create_coding_answer_execution(solution, self.coding_question_id)
 
-        response_data = {
-            'test_cases_passed': num_test_cases_passed,
-            'test_cases_failed': num_test_cases_failed
-        }
+        await sync_to_async(create_quiz_build_runner)(coding_answer_execution)
 
-        await self.send(text_data=json.dumps(response_data))
+        response: CodeRunResponse = await sync_to_async(mark_execution)(coding_answer_execution)
+
+        print(response.model_dump_json())
+
+        await self.send(text_data=response.model_dump_json())
