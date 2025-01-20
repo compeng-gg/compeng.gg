fe3b1d0a4f815aae7d27d4814a289b5b3a0ffc06
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-01-20 00:40:12 -0500

    Change websocket url scheme

--- backend/api/v0/websocket_urls.py
+++ backend/api/v0/websocket_urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 import courses.quizzes.api.websockets as quizzes_websocket_api
 
 websocket_urlpatterns = [
-    path('api/ws/v0/<slug:course_slug>/quiz/<slug:quiz_slug>/run_code/<uuid:coding_question_id>/', quizzes_websocket_api.CodeRunConsumer.as_asgi()),
+    path('api/v0/ws/<slug:course_slug>/quiz/<slug:quiz_slug>/run_code/<uuid:coding_question_id>/', quizzes_websocket_api.CodeRunConsumer.as_asgi()),
 ]
