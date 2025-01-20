e87e3695d45c7f8528d61c2669c369dd8dfb05cf
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-01-20 00:42:08 -0500

    Mock out running code locally

--- backend/runner/management/commands/quiz_buildrunner.py
+++ backend/runner/management/commands/quiz_buildrunner.py
@@ -2,10 +2,12 @@ import pathlib
 import subprocess
 
 from compeng_gg.django.github.models import Push
-from courses.models import CodingAnswerExecution, CodingQuestionTestCaseExecution
+from courses.models import CodingAnswerExecution
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-import shutil
+import json
+import os
+from time import time
 
 class Command(BaseCommand):
 
@@ -22,72 +24,49 @@ class Command(BaseCommand):
         solution = coding_answer_execution.solution
         print(f'Solution is {solution}')
 
-        repository = coding_answer_execution.coding_question.repository
+        repository = coding_answer_execution.coding_question.quiz.repository
 
-        tmp_dir = pathlib.Path("/tmp")
+        # TODO: dont use this path. We're doing this to mock is locally for now
+        curr_time = str(int(time()))
+        tmp_dir = pathlib.Path(f"/tmp/{curr_time}--{coding_answer_execution_id}")
+
+        os.mkdir(tmp_dir)
 
         repo_dir = tmp_dir / repository.name
 
-        try:
-            shutil.rmtree(repo_dir)
-            print("Directory deleted successfully!")
-        except FileNotFoundError:
-            print("Directory not found.")
+        subprocess.run(
+            ["git", "clone", "--depth", "1", f"https://github.com/{repository.full_name}"], 
+            cwd=tmp_dir, 
+            check=True
+        )
 
-        subprocess.run(["git", "clone", "--depth", "1", f"git@github.com:{repository.full_name}"], cwd=tmp_dir)
-
-        # TODO: uncomment this / figure out how to test this
-        registry_url = "localhost:6000" if settings.RUN_LOCALLY else "gitea.eyl.io/jon"
-
-        tag = f"{registry_url}/{repository.name}:latest"
-
-        print(f"Pushing tag {tag}")
-        #subprocess.run(["podman", "build", "-t", tag, "."], cwd=repo_dir)
-        #subprocess.run(["podman", "push", "--tls-verify=false", tag])
-
-        print(f"Repo dir is {repo_dir}")
-        ### Replace a specifc file in the repo with the solution
-
-        # TODO: do this dynamically from models. Currently this only works for the single demo repo
-        file_path_to_replace = "add.py"
+        file_path_to_replace = coding_answer_execution.coding_question.file_to_replace
 
         file_path = f"{repo_dir}/{file_path_to_replace}"
 
         with open(file_path, "w") as file:
             file.write(solution)
 
-        print("Replaced content!")
 
-        ## TODO: we also need to remove the content of EVERY file that is marked as an "answer" file.
-        # If we don't do this, students will be able to extract the correct solutions via std(out/err). Which is bad
-        # This does force instructors not to depend answer files on other answer files. Which should be true, but should be validated?
+        # TODO: launch this in the quiz container
 
-        # ALTERNATIVELY: every question is it's own repo?? Maybe this makes more sense
-        # Currently doing this.
+        grade_file = repo_dir / "grade.py"
 
-        subprocess.run(["podman", "build", "-t", "testcontainer", "."], cwd=repo_dir)
+        res2 = subprocess.run(['python', grade_file], capture_output=True, text=True)
 
-        test_cases = coding_answer_execution.coding_question.test_cases.all()
-        print(len(test_cases))
+        actual_stdout = res2.stdout
 
-        for test_case in test_cases:
-            print(f"Running testcase command {test_case.command}")
-            res = subprocess.run(["podman", "run", "--rm", "testcontainer", "sh", "-c", test_case.command], capture_output=True, text=True)
+        print(f"Actual stdout is {actual_stdout}")
+        print(f"Stderr is {res2.stderr}")
 
-            actual_stdout = res.stdout.strip()
-            print(type(actual_stdout))
-            print(type(test_case.expected_stdout))
-            print(f"stdout: {res.stdout.strip()}")
-            print(f"stderr: {res.stderr.strip()}")
+        try:
+            json_result = json.loads(actual_stdout)
+            coding_answer_execution.result = json_result
+            coding_answer_execution.status = CodingAnswerExecution.Status.SUCCESS
+        except Exception:
+            coding_answer_execution.status = CodingAnswerExecution.Status.FAILURE
 
-            if actual_stdout!= test_case.expected_stdout:
-                print(f"Wrong! Expected {test_case.expected_stdout}")
-            else:
-                print("Right")
+        coding_answer_execution.stderr = res2.stderr
+        coding_answer_execution.save()
 
-            CodingQuestionTestCaseExecution.objects.create(
-                stdout=actual_stdout,
-                stderr=res.stderr,
-                test_case=test_case,
-                coding_answer_execution=coding_answer_execution,
-            )
+        print("Saved!!")
