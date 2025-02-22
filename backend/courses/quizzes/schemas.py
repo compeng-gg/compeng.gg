e6b664bdc4e71c450e5ea312c2b77d949aa9a4ac
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-02-22 12:17:35 -0500

    format schemas

--- backend/courses/quizzes/schemas.py
+++ backend/courses/quizzes/schemas.py
@@ -1,70 +1,105 @@
 from rest_framework import serializers
 import courses.models as db
-from typing import (
-    List,
-    Dict,
-    Any,
-    Optional
-)
+from typing import List, Dict, Any, Optional
 
 
 class MultipleChoiceQuestionSerializer(serializers.ModelSerializer):
-    question_type = serializers.CharField(default='MULTIPLE_CHOICE', read_only=True)
+    question_type = serializers.CharField(default="MULTIPLE_CHOICE", read_only=True)
     selected_answer_index = serializers.SerializerMethodField()
 
     class Meta:
         model = db.MultipleChoiceQuestion
-        fields = ['order', 'prompt', 'question_type', 'points', 'options', 'selected_answer_index', 'id']
+        fields = [
+            "order",
+            "prompt",
+            "question_type",
+            "points",
+            "options",
+            "selected_answer_index",
+            "id",
+        ]
 
-    def get_selected_answer_index(self, multiple_choice_question: db.MultipleChoiceQuestion) -> int:
+    def get_selected_answer_index(
+        self, multiple_choice_question: db.MultipleChoiceQuestion
+    ) -> Optional[int]:
         if (answer := multiple_choice_question.answers.first()) is None:
             return None
-        
+
         return answer.selected_answer_index
 
 
 class CheckboxQuestionSerializer(serializers.ModelSerializer):
-    question_type = serializers.CharField(default='CHECKBOX', read_only=True)
+    question_type = serializers.CharField(default="CHECKBOX", read_only=True)
     selected_answer_indices = serializers.SerializerMethodField()
 
     class Meta:
         model = db.CheckboxQuestion
-        fields = ['order', 'prompt', 'question_type', 'points', 'options', 'selected_answer_indices', 'id']
+        fields = [
+            "order",
+            "prompt",
+            "question_type",
+            "points",
+            "options",
+            "selected_answer_indices",
+            "id",
+        ]
 
-    def get_selected_answer_indices(self, checkbox_question: db.CheckboxQuestion) -> List[int]:
+    def get_selected_answer_indices(
+        self, checkbox_question: db.CheckboxQuestion
+    ) -> Optional[List[int]]:
         if (answer := checkbox_question.answers.first()) is None:
             return None
-        
+
         return answer.selected_answer_indices
 
 
 class WrittenResponseQuestionSerializer(serializers.ModelSerializer):
-    question_type = serializers.CharField(default='WRITTEN_RESPONSE', read_only=True)
+    question_type = serializers.CharField(default="WRITTEN_RESPONSE", read_only=True)
     response = serializers.SerializerMethodField()
 
     class Meta:
         model = db.WrittenResponseQuestion
-        fields = ['order', 'prompt', 'question_type', 'points', 'max_length', 'question_type', 'response', 'id']
+        fields = [
+            "order",
+            "prompt",
+            "question_type",
+            "points",
+            "max_length",
+            "question_type",
+            "response",
+            "id",
+        ]
 
-    def get_response(self, written_response_question: db.WrittenResponseQuestion) -> str:
+    def get_response(
+        self, written_response_question: db.WrittenResponseQuestion
+    ) -> Optional[str]:
         if (answer := written_response_question.answers.first()) is None:
             return None
-        
+
         return answer.response
 
 
 class CodingQuestionSerializer(serializers.ModelSerializer):
-    question_type = serializers.CharField(default='CODING', read_only=True)
+    question_type = serializers.CharField(default="CODING", read_only=True)
     solution = serializers.SerializerMethodField()
 
     class Meta:
         model = db.CodingQuestion
-        fields = ['order', 'prompt', 'points', 'programming_language', 'question_type', 'starter_code', 'solution', 'id']
+        fields = [
+            "order",
+            "prompt",
+            "points",
+            "programming_language",
+            "question_type",
+            "starter_code",
+            "solution",
+            "id",
+        ]
 
-    def get_solution(self, coding_question: db.CodingQuestion) -> str:
+    def get_solution(self, coding_question: db.CodingQuestion) -> Optional[str]:
         if (answer := coding_question.answers.first()) is None:
             return None
-        
+
         return answer.solution
 
 
@@ -75,14 +110,14 @@ class QuizSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = db.Quiz
-        fields = ['title', 'end_unix_timestamp', 'start_unix_timestamp', 'questions']
+        fields = ["title", "end_unix_timestamp", "start_unix_timestamp", "questions"]
 
     def get_questions(self, quiz: db.Quiz) -> List[Dict[str, Any]]:
         checkbox_questions = quiz.checkbox_questions
         multiple_choice_questions = quiz.multiple_choice_questions
         written_response_questions = quiz.written_response_questions
         coding_questions = quiz.coding_questions
-       
+
         serialized_multiple_choice_questions = MultipleChoiceQuestionSerializer(
             multiple_choice_questions,
             many=True,
@@ -94,40 +129,47 @@ class QuizSerializer(serializers.ModelSerializer):
         ).data
 
         serialized_written_response_questions = WrittenResponseQuestionSerializer(
-            written_response_questions, 
+            written_response_questions,
             many=True,
         ).data
 
         serialized_coding_questions = CodingQuestionSerializer(
-            coding_questions, 
+            coding_questions,
             many=True,
         ).data
-        
-        all_questions = serialized_multiple_choice_questions + serialized_checkbox_questions + serialized_written_response_questions + serialized_coding_questions
 
-        sorted_questions = sorted(all_questions, key=lambda question: question['order'])
-        
+        all_questions = (
+            serialized_multiple_choice_questions
+            + serialized_checkbox_questions
+            + serialized_written_response_questions
+            + serialized_coding_questions
+        )
+
+        sorted_questions = sorted(all_questions, key=lambda question: question["order"])
+
         for question in sorted_questions:
-            question.pop('order')
+            question.pop("order")
 
         return sorted_questions
-    
+
     def get_end_unix_timestamp(self, quiz: db.Quiz) -> int:
         return int(quiz.ends_at.timestamp())
-    
+
     def get_start_unix_timestamp(self, quiz: db.Quiz) -> int:
         return int(quiz.starts_at.timestamp())
 
 
 class AnswerMultipleChoiceQuestionRequestSerializer(serializers.Serializer):
     selected_answer_index = serializers.IntegerField(required=True)
-    
+
     def validate_selected_answer_index(self, selected_answer_index: int) -> int:
         if selected_answer_index < 0:
-            raise serializers.ValidationError('The selected answer index must not be negative')
-        
+            raise serializers.ValidationError(
+                "The selected answer index must not be negative"
+            )
+
         return selected_answer_index
-    
+
 
 class AnswerWrittenResponseQuestionRequestSerializer(serializers.Serializer):
     response = serializers.CharField(required=True)
@@ -140,9 +182,11 @@ class AnswerCodingQuestionRequestSerializer(serializers.Serializer):
 def validate_list_is_set(input_list: Optional[List[int]]) -> Optional[List[int]]:
     if input_list is None:
         return None
-    
+
     if len(input_list) != len(set(input_list)):
-        raise serializers.ValidationError('Input list must not contain duplicate values')
+        raise serializers.ValidationError(
+            "Input list must not contain duplicate values"
+        )
 
     return input_list
 
@@ -161,22 +205,17 @@ class CourseQuizzesListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = db.Quiz
-        fields = [
-            'title', 
-            'slug',
-            'start_unix_timestamp',
-            'end_unix_timestamp'
-        ]
+        fields = ["title", "slug", "start_unix_timestamp", "end_unix_timestamp"]
 
     def get_start_unix_timestamp(self, quiz: db.Quiz) -> int:
         return int(quiz.starts_at.timestamp())
 
     def get_end_unix_timestamp(self, quiz: db.Quiz) -> int:
         return int(quiz.ends_at.timestamp())
-    
+
 
 class AllQuizzesListSerializer(CourseQuizzesListSerializer):
-    course_slug = serializers.CharField(source='offering.course.slug', read_only=True)
-    
+    course_slug = serializers.CharField(source="offering.course.slug", read_only=True)
+
     class Meta(CourseQuizzesListSerializer.Meta):
-        fields = CourseQuizzesListSerializer.Meta.fields + ['course_slug']
+        fields = CourseQuizzesListSerializer.Meta.fields + ["course_slug"]
