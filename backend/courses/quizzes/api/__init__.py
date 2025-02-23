2c0cce55e9404b079014a0a311501aa2d62db115
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-02-22 20:32:00 -0500

    Implement quiz question deletion

--- backend/courses/quizzes/api/__init__.py
+++ backend/courses/quizzes/api/__init__.py
@@ -10,26 +10,37 @@ from courses.quizzes.api.submit_written_response_answer import (
 )
 from courses.quizzes.api.list import list_all, list_for_course
 from courses.quizzes.api.admin.api import create_quiz
-from courses.quizzes.api.admin.create.create_multiple_choice_question import (
+from courses.quizzes.api.admin.create_question.create_multiple_choice_question import (
     create_multiple_choice_question,
 )
-from courses.quizzes.api.admin.create.create_checkbox_question import (
+from courses.quizzes.api.admin.create_question.create_checkbox_question import (
     create_checkbox_question,
 )
-from courses.quizzes.api.admin.create.create_written_response_question import (
+from courses.quizzes.api.admin.create_question.create_written_response_question import (
     create_written_response_question,
 )
-from courses.quizzes.api.admin.create.create_coding_question import (
+from courses.quizzes.api.admin.create_question.create_coding_question import (
     create_coding_question,
 )
-from courses.quizzes.api.admin.edit.edit_checkbox_question import edit_checkbox_question
+from courses.quizzes.api.admin.edit_question.edit_checkbox_question import (
+    edit_checkbox_question,
+)
 
-from courses.quizzes.api.admin.edit.edit_coding_question import edit_coding_question
+from courses.quizzes.api.admin.edit_question.edit_coding_question import (
+    edit_coding_question,
+)
 
-from courses.quizzes.api.admin.edit.edit_written_response_question import (
+from courses.quizzes.api.admin.edit_question.edit_written_response_question import (
     edit_written_response_question,
 )
 
-from courses.quizzes.api.admin.edit.edit_multiple_choice_question import (
+from courses.quizzes.api.admin.edit_question.edit_multiple_choice_question import (
     edit_multiple_choice_question,
+)
+
+from courses.quizzes.api.admin.question.delete import (
+    delete_coding_question,
+    delete_multiple_choice_question,
+    delete_checkbox_question,
+    delete_written_response_question,
 )
