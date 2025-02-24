6658ea62e98c6502e710a91fea86290538dad66e
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-02-23 19:15:09 -0500

    Implementation of get quizzes + tests

--- backend/courses/quizzes/api/__init__.py
+++ backend/courses/quizzes/api/__init__.py
@@ -48,3 +48,5 @@ from courses.quizzes.api.admin.question.delete import (
 from courses.quizzes.api.admin.delete_quiz import delete_quiz
 
 from courses.quizzes.api.admin.edit_quiz import edit_quiz
+
+from courses.quizzes.api.admin.get_quiz import get_quiz
---
+++ backend/courses/quizzes/api/admin/get_quiz.py
@@ -0,0 +1,50 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions, status
+from rest_framework.response import Response
+import courses.models as db
+from django.forms.models import model_to_dict
+
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def get_quiz(request, course_slug: str, quiz_slug: str):
+    # TODO: validations
+    quiz = db.Quiz.objects.get(slug=quiz_slug, offering__course__slug=course_slug)
+    quiz_data = model_to_dict(quiz)
+
+    quiz_data["starts_at"] = int(quiz_data["starts_at"].timestamp())
+    quiz_data["ends_at"] = int(quiz_data["ends_at"].timestamp())
+    quiz_data["visible_at"] = int(quiz_data["visible_at"].timestamp())
+
+    quiz_data.pop("repository")
+
+    # TODO: do this all in 1 query
+    checkbox_questions = db.CheckboxQuestion.objects.filter(quiz=quiz).all()
+    coding_questions = db.CodingQuestion.objects.filter(quiz=quiz).all()
+    multiple_choice_questions = db.MultipleChoiceQuestion.objects.filter(quiz=quiz).all()
+    written_response_questions = db.WrittenResponseQuestion.objects.filter(quiz=quiz).all()
+
+    question_types = [
+        checkbox_questions,
+        coding_questions,
+        multiple_choice_questions,
+        written_response_questions,
+    ]
+
+    questions = []
+
+    for question_type in question_types:
+        for question in question_type:
+            data = model_to_dict(question)
+            data.pop("quiz")
+
+            questions.append(data)
+
+    questions = sorted(questions, key=lambda x: x['order'])
+
+
+    quiz_data["questions"] = questions
+    quiz_data["github_repository"] = quiz.repository.full_name
+    
+
+    return Response(status=status.HTTP_200_OK, data=quiz_data)
