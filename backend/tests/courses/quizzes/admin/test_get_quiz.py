6a28c5cccd1c5aafe17991fb74dd397c763d1dd8
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-02-23 19:15:09 -0500

    Implementation of get quizzes + tests

---
+++ backend/tests/courses/quizzes/admin/test_get_quiz.py
@@ -0,0 +1,22 @@
+from tests.utils import TestCasesWithUserAuth, create_quiz, create_checkbox_question, create_coding_question, create_written_response_question, create_multiple_choice_question
+import responses
+from rest_framework import status
+
+
+class GetQuizTests(TestCasesWithUserAuth):
+    def get_api_endpoint(self, course_slug: str, quiz_slug: str) -> str:
+        return f"/api/v0/quizzes/admin/{course_slug}/{quiz_slug}/"
+
+    @responses.activate
+    def test_happy_path(self):
+        mock_quiz = create_quiz(self.user.id, quiz_title="Quiz Title")
+        
+        create_checkbox_question(mock_quiz, order=2)
+        create_coding_question(mock_quiz, order=3)
+        create_written_response_question(mock_quiz, order=4)
+        create_multiple_choice_question(mock_quiz, order=1)
+
+        response = self.client.get(self.get_api_endpoint(mock_quiz.offering.course.slug, mock_quiz.slug))
+        print(response.json())
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
